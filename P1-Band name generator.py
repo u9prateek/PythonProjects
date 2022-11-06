a314ff85ad5a18ec689ac7446bd52ac49c3db657
Prateek Upadhyay <prateeku9@gmail.com>
2022-11-07 03:33:11 +0530

    First Project P1

---
+++ P1-Band name generator.py
@@ -0,0 +1,6 @@
+# Welcome Message
+print("Welcome to the Band Name Generator")
+city = input("Name of city you grew up in?\n")
+pet = input("Name of your pet?\n")
+# combining city and pet name
+print(f"Your band name could be {city} {pet}")
